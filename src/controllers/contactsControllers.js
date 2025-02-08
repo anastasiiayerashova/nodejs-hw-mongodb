428fd6928837ed6042f7f833bbbbcb10034c5f6f
Anastasiia Yerashova <yerashova.a@gmail.com>
2025-02-08 14:10:29 +0100

    update

--- src/controllers/contactsControllers.js
+++ src/controllers/contactsControllers.js
@@ -25,6 +25,10 @@ const getAllController = async (req, res, next) => {
 const getByIdController = async (req, res, next) => {
         const {contactId} = req.params
         const contact = await getContactById(contactId, req.user._id)
+    
+        if (!contact) {
+          throw createHttpError(404, 'Contact not found')
+        }
 
         res.status(200).json({ status: 200, message: `Successfully found contact with id ${contactId}`, data: contact })
 }
